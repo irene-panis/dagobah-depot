6f6a006391c079403027cb4216ac30bcde6d2186
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-07-11 21:34:35 -0700

    when i deleted the api on the controllers folder it deleted all the material inside the folder pasted everthing back into the file

--- contollers/api/index.js
+++ contollers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./projectRoutes');
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
+
