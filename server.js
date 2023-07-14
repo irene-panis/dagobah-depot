5b2fa9ec188c536d0613d3d2f48c34f7446bd566
irene <panisirene@gmail.com>
2023-07-13 19:32:34 -0700

    fix typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 /*
 const sequelizeStore = require('connect-session-sequelize')(session.store);
