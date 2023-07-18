b2b56565a91416b9f5575576940225dcde1185ea
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-07-17 19:44:37 -0700

    added the npm package for stripe and also a dev dependency called nodemon

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 
 /*
