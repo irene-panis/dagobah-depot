ad8cc9863357a0636dfaafe86d8100dbe4633583
EgRoku <jimenezjj5150@gmail.com>
2023-07-20 20:09:05 -0700

    added darkmode switch. added planet logo

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.static("images"));
 
 app.use(routes);
 
