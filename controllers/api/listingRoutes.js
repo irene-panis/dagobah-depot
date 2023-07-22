303b0ac3eb585ffae5e8235dc18e70b495bc3cc8
EgRoku <jimenezjj5150@gmail.com>
2023-07-21 23:05:23 -0700

    added styling to postedItems.handlebars. Found bug on profile page that wouldnt properly GET postedItems when clicked, fixed it

--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -16,7 +16,7 @@ router.post('/post', async (req, res) => {
   }
 });
 
-// delete item NOT WORKING RN
+// delete item
 router.delete('/:id', async (req, res) => {
   try {
     console.log("delete request attempted");
@@ -33,6 +33,57 @@ router.delete('/:id', async (req, res) => {
     }
     res.status(200).send(); // send ok status from server so js can refresh page
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one listing
+router.get('/listings/:id', async (req, res) => {
+  try {
+    const dbListingData = await Listing.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+        },
+      ],   
+    });
+    const listing = dbListingData.get({ plain: true });
+    res.render('postedItems', { // individual product page
+      listing,
+      user_id: req.session.user_id,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/category/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Listing.findAll({
+      where: {
+        category_id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Category,
+        },
+      ],
+    });
+    const listings = dbCategoryData.map((listing) => 
+      listing.get({ plain: true })
+    );
+    listings.reverse();
+    res.render('category', {
+      listings,
+      user_id: req.session.user_id,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
